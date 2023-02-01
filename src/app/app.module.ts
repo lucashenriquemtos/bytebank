ff0e98520b184e3244ae2f43568a15f678dacd5f
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-01 16:09:05 -0300

    Corrigindo import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
-import { FormsModule } from 'angular/forms'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
