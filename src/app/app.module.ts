087247261bf1171531bfac884f3b1679bcb99e9a
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-01 16:03:52 -0300

    Adicionando FormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { FormsModule } from 'angular/forms'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
