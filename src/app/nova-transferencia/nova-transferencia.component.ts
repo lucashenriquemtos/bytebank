3847b3c3502e490cf953f1bbdcff5600b1726e9d
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-01 16:27:52 -0300

    Exportando dados com Output

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor?: number;
   destino?: number;
 
-  transferir(){
-  console.log('Nova Tranferencia');
-  console.log("Valor:", this.valor);
-  console.log("Destino:", this.destino);
+  transferir() {
+    console.log('Nova Tranferencia');
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
   }
 }
